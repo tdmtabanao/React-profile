93cb01b6b5d5a4279cd7c423e22d59be4004f824
tdmtabanao <68370190+tdmtabanao@users.noreply.github.com>
2023-09-02 22:42:31 +0800

    navbar updated

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ const App = () => {
     return(
         <div className='App-main'>
             <div className='navbar-container'>
-            <p>DANIEL'S PORTFOLIO</p>
-            </div>
-            <div className='footer-container'>
-                <p>Footer</p>
+            <p className='nav-text'>A|E Furnishing And Designs</p>
+            <br></br>
+            <span className='btn1'>About Us</span>
+            <button className='btn2'>About Us</button>
+            <button className='btn3'>About Us</button>
             </div>
         </div>
     )
